a6d8b9a75aa49d21573fcc400cbd75bfb6877315
Steffen Kötte <steffen.koette@gmail.com>
2021-09-20 00:02:47 -0700

    revert platform change

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftBeanCountImporter",
-    platforms: [
-       .macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
-    ],
     products: [
         .library(
             name: "SwiftBeanCountImporter",
