fa03f7918775ccbed71eab0888bc11e38bef03fa
Steffen Kötte <steffen.koette@gmail.com>
2021-09-25 13:40:16 -0700

    Test coverage command

---
+++ cov.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+swift test --enable-code-coverage
+echo ""
+
+BIN="$(swift build --show-bin-path)"
+FILE="$(find ${BIN} -name '*.xctest')"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    FILE="${FILE}/Contents/MacOS/$(basename $FILE .xctest)"
+fi
+
+$(xcrun -f llvm-cov) show "${FILE}" -instr-profile="${BIN}/codecov/default.profdata" -ignore-filename-regex=".build|Tests" -show-branch-summary=0 -show-instantiations=0 -line-coverage-lt=100
