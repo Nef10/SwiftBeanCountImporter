5778b8138ef5de85880a1b7c63796a9e2f3e9216
Steffen Kötte <steffen.koette@gmail.com>
2021-09-25 19:57:57 -0700

    Adjust script

--- cov.sh
+++ cov.sh
@@ -5,11 +5,11 @@ echo ""
 
 BIN="$(swift build --show-bin-path)"
 FILE="$(find ${BIN} -name '*.xctest')"
-COV="$(dirname "$(realpath "$(which swift)")")/llvm-cov"
+COV="$(dirname "$(which swift)")/llvm-cov"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     FILE="${FILE}/Contents/MacOS/$(basename $FILE .xctest)"
     COV="$(xcrun -f llvm-cov)"
 fi
 
-$COV show "${FILE}" -instr-profile="${BIN}/codecov/default.profdata" -ignore-filename-regex=".build|Tests" -show-branch-summary=0 -show-instantiations=0 -line-coverage-lt=100
+$COV report "${FILE}" -instr-profile="${BIN}/codecov/default.profdata" -ignore-filename-regex=".build|Tests" -show-branch-summary=0
