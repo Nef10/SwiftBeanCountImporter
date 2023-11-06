ed059fdaf6bb9f3477e461c4857ebd917f94b17e
Steffen Kötte <steffen.koette@gmail.com>
2023-11-05 20:26:12 -0800

    fix linter error

--- Sources/SwiftBeanCountImporter/Importers/ManuLifeImporter.swift
+++ Sources/SwiftBeanCountImporter/Importers/ManuLifeImporter.swift
@@ -225,11 +225,11 @@ class ManuLifeImporter: BaseImporter, TransactionBalanceTextImporter {
     ///   - commodities: dictionary of name to account for commodities
     /// - Returns: Tupel with ManuLifeBuys and the purchase date
     private func parsePurchase(string input: String, commodities: [String: String]) -> ([ManuLifeBuy], Date?) {
-        let purchasePattern = #"\s*.*?\.gif\s*(\d{4}.*?[a-z]\d)\s*$\s*[a-zA-z]*[ ]?(?:Contribution|Fund Transfer)\s*([0-9.,]*)\s*units\s*@\s*\$([0-9.,]*)/unit\s*(-?[0-9.,]*)\s*$"#
+        let pattern = #"\s*.*?\.gif\s*(\d{4}.*?[a-z]\d)\s*$\s*[a-zA-z]*[ ]?(?:Contribution|Fund Transfer)\s*([0-9.,]*)\s*units\s*@\s*\$([0-9.,]*)/unit\s*(-?[0-9.,]*)\s*$"#
 
         // swiftlint:disable force_try
         let dateRegex = try! NSRegularExpression(pattern: #"^(.*?) [a-zA-z]*[ ]?(?:Contribution|Fund Transfer) \(Ref."#, options: [.anchorsMatchLines])
-        let regex = try! NSRegularExpression(pattern: purchasePattern, options: [.anchorsMatchLines])
+        let regex = try! NSRegularExpression(pattern: pattern, options: [.anchorsMatchLines])
         // swiftlint:enable force_try
 
         // Parse purchase date
