94bac4634f0a117e08f814a4a5a9b188c282c83d
Steffen Kötte <steffen.koette@gmail.com>
2024-12-16 16:10:09 +0100

    Downgrade CSV.swift package version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/yaslab/CSV.swift.git",
-            from: "2.5.2"
+            from: "2.4.3"
         ),
         .package(
             url: "https://github.com/Nef10/SwiftBeanCountParserUtils.git",
